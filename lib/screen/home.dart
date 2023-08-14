87f4ca09771aaed818b182568ce1eadf21d1b0cc
이하나 <127467192+LeEHaNa1@users.noreply.github.com>
2023-08-14 18:20:24 +0900

    6.6

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,36 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutterwebtoon/models/webtoonmodel.dart';
 import 'package:flutterwebtoon/services/api_service.dart';
+import 'package:flutterwebtoon/models/webtoonmodel.dart';
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+class Home extends StatelessWidget {
+  Home({Key? key}) : super(key: key);
 
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebtoons() async {
-    webtoons = await ApiService.getTodaysToon();
-    isLoading = false;
-    setState(() {});
-  }
-//setState의 역할:
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebtoons();
-  } //initState의 역할: 상태를 초기화시킴.(build 동작 이전에.)
-  //상태가 초기화되면서 waitForWebtoons()가 실행되어 getTodaysToon()로 data를 받아 webtoons에 넣고 isLoading을 false로 두었다.
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToon();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -41,7 +19,15 @@ class _HomeState extends State<Home> {
             child: Text("Today's webtoon",
                 style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500))),
       ),
-      //body:
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Text("There is Data!");
+          }
+          return const Text("Loading");
+        },
+      ),
     );
   }
 }
